5213c4cbe796dcf7273de55643cd238def252399
airasheed <abdullah@airasheed.com>
2015-10-21 08:56:00 -0500

    added heroku mongolab config variable

--- config/env/development.js
+++ config/env/development.js
@@ -2,6 +2,6 @@
 //Set the 'development' environment configuartion object
 
 module.exports = {
-    db: process.env.MONGOLAB_URI,
+    db: process.env.PROD_MONGODB,
     sessionSecret : '59B93087-78BC-4EB9-993A-A61FC844F6C9'
 };
