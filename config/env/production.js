7d87bef7481bef8d05d2d2df5986e654ee34a8ac
airasheed <abdullah@airasheed.com>
2015-10-21 10:27:37 -0500

    changed prdouction db string

--- config/env/production.js
+++ config/env/production.js
@@ -2,6 +2,6 @@
 //Set the 'development' environment configuartion object
 
 module.exports = {
-    db: 'mongodb://airasheed:airasheed@ds039484.mongolab.com:39484/mean--todo',
+    db: process.env.PROD_MONGODB,
     sessionSecret : '59B93087-78BC-4EB9-993A-A61FC844F6C9'
 };
